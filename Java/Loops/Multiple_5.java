ad28121bbad41d9b8ca7fcf72c90d6121da48b92
Suhana <suhananejakar2001@gmail.com>
2026-02-05 16:25:47 +0530

    added prb

---
+++ Java/Loops/Multiple_5.java
@@ -0,0 +1,8 @@
+class Multiple_5 {
+    public static void main(String[] args) {
+        for(int i = 1; i<= 10; i++){
+        System.out.println("5*" + i + "=" + (5*i));
+        }
+    }
+    
+}
---
+++ Java/Loops/OnetoTen.java
@@ -0,0 +1,7 @@
+class OnetoTen{
+    public static void main(String[] args){
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
